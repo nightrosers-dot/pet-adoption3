5f83ca84f5fd24ad06fc35d31b3b9d27382d0ff2
nightrosers-dot <nightrosers@gmail.com>
2025-12-26 15:57:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 async function start() {
-    const weatherDataPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
-    const weatherData = await weatherPromise.json()
+    try {
+        const weatherDataPromise = await fetch(
+            "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
+        );
+        const weatherData = await weatherDataPromise.json();
 
-    const ourTemperature = weatherData.properties.periods[0].temperature
-    document.querySelector("#temperature-output").textContent = ourTemperature
+        const ourTemperature =
+            weatherData.properties.periods[0].temperature;
+
+        document.querySelector("#temperature-output").textContent =
+            ourTemperature;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-start()
+start();
